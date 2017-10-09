faafb87be4dc36f4081c335f04ec90262728696a
ehtisham.haq <ehtisham.haq@evampsaanga.com>
2017-10-09 23:12:51 +0500

    Kotlin Code changes implemented

--- app/src/main/java/com/example/android/myaddressbook/ContactsActivity.kt
+++ app/src/main/java/com/example/android/myaddressbook/ContactsActivity.kt
@@ -6,13 +6,11 @@ import android.content.Context
 import android.content.Loader
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import android.support.v7.widget.helper.ItemTouchHelper
 import android.text.Editable
 import android.text.TextWatcher
@@ -23,6 +21,10 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_contacts.*
+import kotlinx.android.synthetic.main.contact_list_item.view.*
+import kotlinx.android.synthetic.main.content_contacts.*
+import kotlinx.android.synthetic.main.input_contact_dialog.view.*
 import org.json.JSONArray
 import org.json.JSONException
 import java.util.*
@@ -44,8 +46,6 @@ class ContactsActivity : AppCompatActivity(), TextWatcher, LoaderManager.LoaderC
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
 
         mPrefs = getPreferences(Context.MODE_PRIVATE)
         mContacts = loadContacts()
@@ -88,10 +88,8 @@ class ContactsActivity : AppCompatActivity(), TextWatcher, LoaderManager.LoaderC
      * Sets up the RecyclerView: empty data set, item dividers, swipe to delete.
      */
     private fun setupRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.contact_list)
-
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        recyclerView.adapter = mAdapter
+        contact_list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        contact_list.adapter = mAdapter
 
         // Implements swipe to delete
         val helper = ItemTouchHelper(
@@ -112,7 +110,7 @@ class ContactsActivity : AppCompatActivity(), TextWatcher, LoaderManager.LoaderC
                     }
                 })
 
-        helper.attachToRecyclerView(recyclerView)
+        helper.attachToRecyclerView(contact_list)
     }
 
     /**
@@ -128,9 +126,9 @@ class ContactsActivity : AppCompatActivity(), TextWatcher, LoaderManager.LoaderC
         val dialogView = LayoutInflater.from(this)
                 .inflate(R.layout.input_contact_dialog, null)
 
-        mFirstNameEdit = dialogView.findViewById(R.id.edittext_firstname)
-        mLastNameEdit = dialogView.findViewById(R.id.edittext_lastname)
-        mEmailEdit = dialogView.findViewById(R.id.edittext_email)
+        mFirstNameEdit = dialogView.edittext_firstname
+        mLastNameEdit = dialogView.edittext_lastname
+        mEmailEdit = dialogView.edittext_email
 
         // Listens to text changes to validate after each key press
         mFirstNameEdit.addTextChangedListener(this)
@@ -346,8 +344,8 @@ class ContactsActivity : AppCompatActivity(), TextWatcher, LoaderManager.LoaderC
         }
 
         internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var nameLabel: TextView = itemView.findViewById(R.id.textview_name)
-            var emailLabel: TextView = itemView.findViewById(R.id.textview_email)
+            var nameLabel: TextView = itemView.textview_name
+            var emailLabel: TextView = itemView.textview_email
 
             init {
 
